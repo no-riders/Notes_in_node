7eadfa745b406cade30cec5e3541927af2a767db
Yev <evlogosha@gmail.com>
2017-10-23 16:02:55 +0300

    firts commit

---
+++ app.js
@@ -0,0 +1,28 @@
+//jshint esnext: true
+console.log('-=Notes in Node=-');
+const fs = require('fs');
+const _ = require('lodash');
+const yargs = require('yargs');
+
+const notes = require('./notes.js');
+
+//to work with commands in terminal
+const argv = yargs.argv;
+var command = argv._[0];
+//console.log('Yarg: ', argv);
+console.log('Command: ', command || 'type one of the command listed bellow. Eg app.js list');
+
+//all types of supported commands
+if (command === 'add') {
+	notes.addNote(argv.title, argv.body);	
+} else if (command === 'list') {
+	notes.getAll();
+} else if (command === 'read') {
+	notes.getNote(argv.title);
+} else if (command === 'remove') {
+	notes.removeNote(argv.title);
+} else {
+	console.log('**********');
+    console.log('To create note, type: "add --title "your_title" --body "body_of_your_note""\nTo view all saved notes: "list"\nTo get specific note by title: "read --title "note_title""\nTo delete specific note: "remove --title "note_title""');
+	console.log('**********');
+}
---
+++ notes.js
@@ -0,0 +1,74 @@
+//jshint esnext: true
+
+const fs = require('fs');
+
+//create note
+let addNote = (title, body) => {
+    let originalNote = {
+        title: '',
+        body: ''
+    };
+
+    let newNote = Object.create(originalNote);
+    newNote.title = title;
+    newNote.body = body;
+
+    let originalNoteString = JSON.stringify(newNote);
+
+    //write JSON value from originalNoteString to this folder
+    fs.writeFileSync(`./notes/${title}.json`, originalNoteString);
+    console.log('**********');
+    console.log(`Note '${title}' has been created\nTo view note use "read --title "note_title""\nTo delete use "remove --title "note_title""`);
+    console.log('**********');
+};
+
+//list all notes saved
+let getAll = () => {
+    //show content of the dir
+    fs.readdir('./notes/', (err, files) => {
+        if (err) throw err;
+        console.log('**********');
+        console.log('Saved notes:');
+        //check if notes presented
+        if (files.length > 0) {
+            files.forEach(item => {
+                //remove file ext from each note and list notes
+                let string = String(item);
+                console.log('- ' + string.slice(0, -5));
+            });
+        } else {
+            console.log('No notes found!\nTo create new note use: "add --title "your_title" --body "body_of_your_note""');
+        }
+        console.log('To view note use "read --title "note_title""\nTo delete use "remove --title "note_title""');
+        console.log('**********');
+    });
+};
+
+//get note by title
+let getNote = (title) => {
+    console.log('**********');
+    console.log(`Getting '${title}'...`);
+    console.log('The body of the note:');
+    let noteString = fs.readFileSync(`./notes/${title}.json`);
+    let note = JSON.parse(noteString);
+    console.log(note.body);
+    console.log('**********');
+};
+
+//to delete note
+let removeNote = (title) => {
+    console.log('**********');
+    console.log(`Note '${title}' has been deleted.`);
+    fs.unlink(`./notes/${title}.json`, (err) => {
+        if (err) throw err;
+    });
+    console.log('**********');
+};
+
+//to make all f() available for app.js
+module.exports = {
+    addNote,
+    getAll,
+    getNote,
+    removeNote
+};
